648704f001c3bee7e25c62b9537677f5d7c59bc8
Emily <emilythid@gmail.com>
2018-05-29 00:16:46 -0400

    lesson 5 p25

--- lesson-5/password-generator/password_generator.py
+++ lesson-5/password-generator/password_generator.py
@@ -3,30 +3,29 @@ import random
 
 """
 Quiz: Password Generator
-Write a function called generate_password that selects three random words from the list of words word_list
-and concatenates them into a single string. Your function should not accept any arguments and should
-reference the global variable word_list to build the password.
-"""
+Write a function called generate_password that selects three random words from the list of words
+word_list and concatenates them into a single string. Your function should not accept any arguments
+and should reference the global variable word_list to build the password."""
 
 word_file = "words.txt"
 word_list = []
 
 #fill up the word_list
-with open(word_file,'r') as words:
-	for line in words:
+with open(word_file, 'r') as words:
+    for line in words:
 		# remove white space and make everything lowercase
-		word = line.strip().lower()
+        word = line.strip().lower()
 		# don't include words that are too long or too short
-		if 3 < len(word) < 8:
-			word_list.append(word)
+        if 3 < len(word) < 8:
+            word_list.append(word)
 
 # Add your function generate_password here
 def generate_password():
     password = []
-    for i in range(3):
+    for _ in range(3):
         password.append(random.choice(word_list))
     password = ''.join(password)
-    return(password)
+    return password
 
 # It should return a string consisting of three random words
 # concatenated together without spaces
