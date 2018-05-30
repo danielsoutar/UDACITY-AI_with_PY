d79cd903d3636d50abf3fc0c8744d787a724b244
Emily <emilythid@gmail.com>
2018-05-30 00:43:03 -0400

    removing lesson-5

--- lesson-5/reading-and-writing-files/example.py
+++
@@ -1,4 +0,0 @@
-with open("camelot.txt") as song:
-    print(song.read(2))
-    print(song.read(8))
-    print(song.read())
--- lesson-5/reading-and-writing-files/read-file.py
+++
@@ -1,8 +0,0 @@
-with open('some_text.txt', 'r') as f:
-    file_data = f.read()
-
-
-#same thing
-#f = open('some_text.txt', 'r')
-#file_data = f.read()
-#f.close()
