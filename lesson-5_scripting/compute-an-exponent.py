b7426967dd3e635bbddcbbca7381943dd14bf1f8
Emily <emilythid@gmail.com>
2018-06-23 23:52:23 -0400

    change folders

--- lesson-5_scripting/compute-an-exponent.py
+++
@@ -1,7 +0,0 @@
-# Quiz: Compute an Exponent
-# It's your turn to import and use the math module. Use the math module to calculate e to the power of 3. print the answer.
-# Refer to the math module's documentation to find the function you need!
-
-# print e to the power of 3 using the math module
-import cmath
-print(cmath.exp(3))
